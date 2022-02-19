c3d891eb1a0d3eae6dcb2e487df6fb76c48b0e97
NKR-28 <91577650+NKR-28@users.noreply.github.com>
2022-02-18 19:42:39 -0800

    Delete flashcard-main directory

--- flashcard-main/db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS budgetbuddy_db;
-CREATE DATABASE budgetbuddy_db;
--- flashcard-main/models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Expense = require('./Expense');
-const Category = require('./Category');
-
-module.exports = { User };
